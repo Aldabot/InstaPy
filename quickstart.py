452ab1b78d7dbf788a0dcc7c12ada690529fa166
knowledge91 <dirkhornung91@gmail.com>
2018-03-03 19:50:20 +0100

    InstaPy for Alda V1.0

--- quickstart.py
+++ quickstart.py
@@ -1,30 +1,46 @@
 from instapy import InstaPy
+import schedule
+import time
 
-insta_username = ''
-insta_password = ''
+insta_username = 'alda.bot'
+insta_password = ''  # fill in password
 
-# set headless_browser=True if you want to run InstaPy on a server
-try:
-    # set these if you're locating the library in the /usr/lib/pythonX.X/ directory
-    # Settings.database_location = '/path/to/instapy.db'
-    # Settings.browser_location = '/path/to/chromedriver'
-    
-    session = InstaPy(username=insta_username,
-                      password=insta_password,
-                      headless_browser=False,
-                      multi_logs=True)
-    session.login()
 
-    # settings
-    session.set_upper_follower_count(limit=2500)
-    session.set_do_comment(True, percentage=10)
-    session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
-    session.set_dont_include(['friend1', 'friend2', 'friend3'])
-    session.set_dont_like(['pizza', 'girl'])
+def job():
+    try:
+        session = InstaPy(username=insta_username,
+                          password=insta_password,
+                          headless_browser=False,
+                          multi_logs=True,
+                          nogui=True)
+        session.login()
 
-    # actions
-    session.like_by_tags(['natgeo'], amount=1)
+        # settings
+        # session.set_upper_follower_count(limit=2500)
+        # session.set_do_comment(True, percentage=10)
+        # session.set_comments(['aMEIzing!', 'So much fun!!', 'Nicey!'])
+        # session.set_dont_include(['friend1', 'friend2', 'friend3'])
+        # session.set_dont_like(['pizza', 'girl'])
 
-finally:
-    # end the bot session
-    session.end()
+        # actions
+        session.like_by_tags(['barcelona', 'madrid'], amount=100)
+        session.end()
+    except:
+        import traceback
+        print(traceback.format_exc())
+
+
+schedule.every().day.at("10:00").do(job)
+schedule.every().day.at("11:30").do(job)
+schedule.every().day.at("13:00").do(job)
+schedule.every().day.at("14:30").do(job)
+schedule.every().day.at("16:00").do(job)
+schedule.every().day.at("17:30").do(job)
+schedule.every().day.at("19:00").do(job)
+schedule.every().day.at("20:30").do(job)
+schedule.every().day.at("22:00").do(job)
+schedule.every().day.at("23:30").do(job)
+
+while True:
+    schedule.run_pending()
+    time.sleep(1)
